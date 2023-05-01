eb906881230216467dd214b6f5b152f90566ab1f
picel <picel@picel.net>
2023-05-01 12:55:08 +0900

    Initialize deck and split them

---
+++ handlers/joker/initializeDeck.ts
@@ -0,0 +1,29 @@
+export default function (player: number) {
+    let deck: string[] = [];
+
+    // add 52 cards
+    for (let j = 1; j <= 13; j++) {
+        let jString: string = j.toString();
+        if (j < 10) {
+            jString = "0" + jString;
+        }
+        deck.push("S" + jString);
+        deck.push("H" + jString);
+        deck.push("C" + jString);
+        deck.push("D" + jString);
+    }
+
+    // shuffle deck
+    deck.sort(() => Math.random() - 0.5);
+    
+    let decks: string[][] = [];
+    for (let i = 0; i < player; i++) {
+        decks.push([]);
+    }
+    for (let i = 0; i < deck.length; i++) {
+        decks[i % player].push(deck[i]);
+    }
+    let joker = Math.floor(Math.random() * player);
+    decks[joker].push("joker");
+    return decks;
+}
---
+++ test/deckInitTest.ts
@@ -0,0 +1,32 @@
+import initializeDeck from "../handlers/joker/initializeDeck";
+
+let Deck: string[][] = initializeDeck(4);
+
+console.log(Deck);
+
+// check if duplicated cards exist
+let check: string[] = [];
+for (let i = 0; i < Deck.length; i++) {
+    for (let j = 0; j < Deck[i].length; j++) {
+        if (check.includes(Deck[i][j])) {
+            console.log("duplicated card exists");
+            console.log(Deck[i][j]);
+        }
+        check.push(Deck[i][j]);
+    }
+}
+
+// check if joker exists
+let jokerExists: boolean = false;
+for (let i = 0; i < Deck.length; i++) {
+    for (let j = 0; j < Deck[i].length; j++) {
+        if (Deck[i][j] === "joker") {
+            jokerExists = true;
+            console.log("joker exists in deck " + i);
+        }
+    }
+}
+
+if (!jokerExists) {
+    console.log("joker does not exist");
+}
